83f2411dabe41a92ae0d122382d5ef7bb3428b96
Angstskrig <Angstskrig@gmail.com>
2022-08-04 18:35:15 +0200

    Exercise 3 done. Math missing in my head!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  if (string === "") {
+    return "";
+  } else {
+    let newString = "";
+    let num = string.length;
+    for (let i = 0; i < num; i++) {
+      newString += string.slice(num - (i + 1), num - i);
+    }
+    return newString;
+  }
 };
 
 // Do not edit below this line
