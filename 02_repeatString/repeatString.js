f9e9c38c6dc76ac8fe8ce6ecd51a3a16eea0420e
Angstskrig <Angstskrig@gmail.com>
2022-08-04 18:18:55 +0200

    First 2 exercises done. Had trouble with the commit - forgot to branch :()

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num === 0) {
+    return (string = "");
+  } else if (num < 0) {
+    return "ERROR";
+  } else {
+    let string2 = "";
+    for (let i = 0; i < num; i++) {
+      string2 += string;
+    }
+    return string2;
+  }
 };
 
 // Do not edit below this line
