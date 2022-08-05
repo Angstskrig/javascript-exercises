3ab437575736a3a27381aae32cafca7f91f7b5b5
Angstskrig <Angstskrig@gmail.com>
2022-08-05 20:34:24 +0200

    Yehaw! All test passed. Had some trouble with where to reset the arrays and how to change the num value. :)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,22 @@
-const removeFromArray = function (array, remove) {
-  if (array === "") {
-    return "";
-  } else {
-    let element = [];
-    let num = array.length;
-    for (const arg of remove) {
-      for (let i = 0; i < num; i++) {
-        if (array[i] !== remove) {
-          element.push(array[i]);
-          console.log(element);
-        }
+const removeFromArray = function (array, ...remove) {
+  let element;
+  let num;
+  for (const arg of remove) {
+    element = [];
+    num = array.length;
+    for (let i = 0; i < num; i++) {
+      if (array[i] !== arg) {
+        element.push(array[i]);
+        // console.log("Element in the for loop = " + element);
       }
     }
-    return element;
+    array = element;
   }
+  // console.log("Final array = " + array);
+  // console.log("Final element = " + element);
+  // console.log("Num = " + num);
+  console.log("Test done!");
+  return element;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
