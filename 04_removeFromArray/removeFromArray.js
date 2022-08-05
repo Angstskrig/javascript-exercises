4a31c761d5fd20671e3d2787c7bc8cc8b1e9c7b0
Angstskrig <Angstskrig@gmail.com>
2022-08-05 20:03:12 +0200

    First save. First part ok - working on second!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, remove) {
+  if (array === "") {
+    return "";
+  } else {
+    let element = [];
+    let num = array.length;
+    for (const arg of remove) {
+      for (let i = 0; i < num; i++) {
+        if (array[i] !== remove) {
+          element.push(array[i]);
+          console.log(element);
+        }
+      }
+    }
+    return element;
+  }
 };
 
 // Do not edit below this line
