99bc416676ea3108a368d4ca7ef581942ada85e3
Angstskrig <Angstskrig@gmail.com>
2022-08-05 21:03:58 +0200

    Had trouble getting the typeof part. Was trying isNaN!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (num1 > num2) {
+    let num3 = num1;
+    num1 = num2;
+    num2 = num3;
+  }
 
+  if (num1 < 1 || num2 < 1) {
+    return "ERROR";
+  }
+
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  }
+
+  let total = num1;
+  for (let i = num1; i < num2; i++) {
+    total += i + 1;
+  }
+  return total;
 };
 
 // Do not edit below this line
