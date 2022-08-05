709e2c1f61515dd1f3d60ab9bc72451a80e7b060
Angstskrig <Angstskrig@gmail.com>
2022-08-05 21:31:51 +0200

    Loved this one. Had to look up the math.round method. :D

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  let celc = Math.round((f - 32) * (5 / 9) * 10) / 10;
+  return celc;
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  let fahr = Math.round((c * (9 / 5) + 32) * 10) / 10;
+  return fahr;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
